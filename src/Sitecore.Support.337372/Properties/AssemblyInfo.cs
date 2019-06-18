aac36467cabb81f5fbc2958db327dab40ef0cde9
iro <iro@sitecore.net>
2019-06-18 20:16:21 +0300

    Initial

---
+++ src/Sitecore.Support.337372/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.337372")]
+[assembly: AssemblyProduct("Sitecore.Support.337372")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.337372/SitemapGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml;
+using Sitecore.Data.Items;
+using Sitecore.Links;
+using Sitecore.XA.Feature.SiteMetadata.Sitemap;
+
+namespace Sitecore.Support.XA.Feature.SiteMetadata.Sitemap
+{
+    public class SitemapGenerator : Sitecore.XA.Feature.SiteMetadata.Sitemap.SitemapGenerator
+    {
+        public SitemapGenerator() :base()
+        {
+        }
+
+        public SitemapGenerator(XmlWriterSettings xmlWriterSettings) :base(xmlWriterSettings)
+        {
+        }
+        
+        protected override string GetFullLink(Item item, SitemapLinkOptions options)
+        {
+            string relativeUrl = LinkManager.GetItemUrl(item, options.UrlOptions);
+            return options.Scheme + Uri.SchemeDelimiter + options.TargetHostname + relativeUrl;
+        }
+    }
+}
